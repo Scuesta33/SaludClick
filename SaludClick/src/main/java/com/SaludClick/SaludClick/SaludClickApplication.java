8074bca8f590dea9e7e7460fdd5959cf2f435e81
sergi <sergi@192.168.1.142>
2024-11-17 20:01:13 +0100

    first commit

---
+++ SaludClick/src/main/java/com/SaludClick/SaludClick/SaludClickApplication.java
@@ -0,0 +1,13 @@
+package com.SaludClick.SaludClick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaludClickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaludClickApplication.class, args);
+	}
+
+}
