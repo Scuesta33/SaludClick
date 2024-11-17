ba84dee55e322bc7db7a01c03b7d6be3f1cb66d4
sergi <sergi@192.168.1.142>
2024-11-17 20:26:36 +0100

    UsuarioRepository

--- SaludClick/src/main/java/com/SaludClick/SaludClick/Repository/UsuarioRepository.java
+++ SaludClick/src/main/java/com/SaludClick/SaludClick/Repository/UsuarioRepository.java
@@ -1,5 +1,10 @@
 package com.SaludClick.SaludClick.Repository;
 
-public interface UsuarioRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.SaludClick.SaludClick.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Usuario findByEmail(String email);  // For login
 }
+
